50ed19b382766b51e3e42bd8dfd32acf068523c7
Bruno Codeman <brunobemfica@gmail.com>
2019-08-14 04:01:41 -0300

    adding query

--- SevenGeesUrlShortener/schema.py
+++ SevenGeesUrlShortener/schema.py
@@ -1,15 +1,25 @@
 import graphene
 from .models import ShortenedUrlModel
 from graphene_django import DjangoObjectType
-from graphene import ObjectType, Mutation, String, Field
+from graphene import ObjectType, Mutation, String, Node, Field
 from .service_module import generate_shorted_url
 
 
 class ShortenedUrl(DjangoObjectType):
     class Meta:
         model = ShortenedUrlModel
+        field = ("original_url", "shortened_url")
 
+class Query:
+    url = Field(ShortenedUrl, shortened_url=String())
+    def resolve_url(self, info, **kwargs):
+        shortened_url = kwargs.get('shortened_url')
+        if shortened_url is not None:
+            return ShortenedUrlModel.objects.get(shortened_url=shortened_url)
+        return None
 
+class Queries(Query, ObjectType):
+    pass
 class CreateShortenedUrl(Mutation):
     class Arguments:
         original_url = String()
@@ -25,4 +35,4 @@ class CreateShortenedUrl(Mutation):
 class UrlMutation(ObjectType):
     create_shortened_url = CreateShortenedUrl.Field()
 
-schema = graphene.Schema(mutation=UrlMutation)
+schema = graphene.Schema(mutation=UrlMutation, query=Queries)
